6ffd26b638e35f734f5ee31028696c5f3033a425
Abid Raza <abid.neduet@gmail.com>
2022-08-04 14:11:38 +0500

    easy buy

--- app.js
+++ app.js
@@ -5,6 +5,37 @@ const dotenv = require('dotenv');
 dotenv.config();
  
 
+const jwt = require('jsonwebtoken');
+var path = require('path');
+var cors = require('cors')
+
+// To access public folder
+app.use(cors())
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json())
+
+
+
+// MULTER
+const multer  = require('multer')
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads/')
+  },
+  filename: function (req, file, cb) {
+    let uploadFile = file.originalname.split('.')
+    let name = `${uploadFile[0]}-${Date.now()}.${uploadFile[uploadFile.length-1]}`
+    cb(null, name)
+  }
+})
+const upload = multer({ storage: storage })
+
+
+
+
+
 
 
 
